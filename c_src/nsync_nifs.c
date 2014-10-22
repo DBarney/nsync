87e4b3e18774845ddafd42228610a171d80f8e6c
Daniel Barney <daniel@pagodabox.com>
2014-10-22 11:03:17 -0600

    removed horrible compatibility code

--- c_src/nsync_nifs.c
+++ c_src/nsync_nifs.c
@@ -22,7 +22,6 @@
 //
 // -------------------------------------------------------------------
 #include "erl_nif.h"
-#include "erl_nif_compat.h"
 #include "lzf.h"
 #include <errno.h>
 
@@ -36,15 +35,15 @@ int decompress1(ErlNifEnv *env, ErlNifBinary *source, ErlNifBinary *target) {
     while(expansion_factor < 6) {
         bufsize = (int) source->size * expansion_factor;
         bufsize = bufsize < 66 ? 66 : bufsize;
-        enif_alloc_binary_compat(env, bufsize, target);
+        enif_alloc_binary(bufsize, target);
         result = lzf_decompress(source->data, source->size, target->data, target->size);
         if (result) {
-            enif_realloc_binary_compat(env, target, result);
+            enif_realloc_binary(target, result);
             retval = 1;
             break;
         }
         else {
-            enif_release_binary_compat(env, target);
+            enif_release_binary(target);
         }
         expansion_factor += 0.1;
     }
@@ -59,15 +58,15 @@ int compress1(ErlNifEnv *env, ErlNifBinary *source, ErlNifBinary *target) {
     while(expansion_factor < 6) {
         bufsize = (int) source->size * expansion_factor;
         bufsize = bufsize < 66 ? 66 : bufsize; 
-        enif_alloc_binary_compat(env, bufsize, target);
+        enif_alloc_binary(bufsize, target);
         result = lzf_compress(source->data, source->size, target->data, target->size);
         if (result) {
-            enif_realloc_binary_compat(env, target, result);
+            enif_realloc_binary(target, result);
             retval = 1;
             break;
         }
         else {
-            enif_release_binary_compat(env, target);
+            enif_release_binary(target);
         }
         expansion_factor += 0.1;
     }
@@ -101,17 +100,17 @@ static ERL_NIF_TERM decompress2_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM
         !enif_inspect_binary(env, argv[0], &source) ||
         !enif_get_uint(env, argv[1], &decompressed_length) ||
         decompressed_length < source.size ||
-        !enif_alloc_binary_compat(env, decompressed_length, &target)) {
+        !enif_alloc_binary(decompressed_length, &target)) {
         return enif_make_badarg(env);
     }
 
     final_length = lzf_decompress(source.data, source.size, target.data, target.size);
     if (final_length > 0 &&
         final_length == decompressed_length) {
-        enif_realloc_binary_compat(env, &target, final_length);
+        enif_realloc_binary(&target, final_length);
         retval = enif_make_binary(env, &target);
     } else {
-        enif_release_binary_compat(env, &target);
+        enif_release_binary(&target);
         retval = enif_make_badarg(env);
     }
     return retval;
@@ -122,7 +121,7 @@ ERL_NIF_TERM compress_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     ErlNifBinary source;
     ErlNifBinary target;
 
-    if (argc != 1 || !enif_inspect_binary(env, argv[0], &source)) {
+    if (argc != 1 || !enif_inspect_binary(env ,argv[0], &source)) {
         return enif_make_badarg(env);
     }
     if (compress1(env, &source, &target)) {
